d560001d021fe711978ee844e73435bfd498fd0e
Johnny Gérard <john@jgerard.dev>
2024-12-20 14:38:21 +0100

    build: configure HTTP headers

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
+        // Prevent search engines from indexing the website
+        // @see https://developers.google.com/search/docs/crawling-indexing/robots-meta-tag#directives
+        {
+          key: "x-robots-tag",
+          value: "none",
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
