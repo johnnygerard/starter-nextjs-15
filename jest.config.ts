3bff81e64ac4b9f954a3b2719e25b386014b6b05
Johnny Gérard <john@jgerard.dev>
2024-12-20 15:00:19 +0100

    build: configure Jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+// @see https://nextjs.org/docs/app/building-your-application/testing/jest
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+};
+
+export default createJestConfig(config);
